ad4955dfe22f2a09c7faef3a267aa24b3af601c6
Richard Wasonga <rowwasonga@gmail.com>
2025-05-16 09:16:04 +0300

    first commit

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,29 @@
 class Author:
-    pass
+
+    all_authors = []
+
+    def __init__(self,name : str):
+        self.name = name
+        Author.all_authors.append(self)
 
 
 class Book:
-    pass
+
+    all_books = []
+
+    def __init__(self,title : str):
+        self.title = title
+        Book.all_books.append(self)
 
 
 class Contract:
-    pass
+
+    all_contracts = []
+
+    def __init__(self,author : Author,book : Book,date : str,royalties : int):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all_contracts.append(self)
+        
